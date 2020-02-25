86995963f9c33f0ea3b518f1621cdff60a3a5753
Ekaterina <nataveres@gmail.com>
2020-02-25 16:47:19 +0100

    Index+init

--- init.js
+++ init.js
@@ -1,6 +1,10 @@
 window.onload = () => {
   debugger;
   // log initial state
+  window.onload = () => {
+    log.push({ initialNumbers: JSON.parse(JSON.stringify(numbers)) });
+    document.getElementById("current-number").innerHTML = numbers.current;
+  };
 
   // render initial UI from state
-}
+};
