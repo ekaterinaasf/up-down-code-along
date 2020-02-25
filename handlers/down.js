00abaa3119a2b9c8c868972240803552b451bd4e
Ekaterina <nataveres@gmail.com>
2020-02-25 16:44:23 +0100

    Down-handler finished

--- handlers/down.js
+++ handlers/down.js
@@ -1,16 +1,27 @@
 function downHandler() {
   debugger;
   // read user input & cast to Number
+  const increment = Number(document.getElementById("increment").value);
 
   // read state
+  const oldCurrent = numbers.current;
 
   // perform logic, combining state with user input
   const newCurrent = oldCurrent - increment;
 
   // update state (.current & .past)
+  numbers.current = newCurrent;
+  numbers.past.push(numbers.current);
 
   // render to user
+  document.getElementById("current-number").innerHTML = numbers.current;
 
   // log user interaction
   //  story name, increment, newCurrent, copy of state
+  log.push({
+    handler: "down",
+    increment,
+    numbers: JSON.parse(JSON.stringify(numbers))
+  });
+  return true;
 }
